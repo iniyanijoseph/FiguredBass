40d6bcc3042e171ac83879da3c0211536d8bb118
PrinceCaspian <princecaspian@duck.com>
2023-09-03 20:38:27 -0500

    Now the iterative deepening actually works

--- Main.java
+++ Main.java
@@ -5,13 +5,14 @@ public class Main {
         Note n = new Note("a", 0, 2);
         Note[][] cTable = Note.generateChordTable(n);
 
-        HashSet<String> seen = new HashSet<>();
         HashSet<String> solutions = new HashSet<>();
 
         Compose l = new Compose(new Chord[] { new Chord(cTable[1], 2, 2, true), new Chord(cTable[4], 5, 2, true) });
 
         for (int i = 1; solutions.size() == 0; i++) {
+            HashSet<String> seen = new HashSet<>();
             l.recur(seen, 1, i, solutions);
+            System.out.println(i);
         }
         System.out.println(solutions);
     }
